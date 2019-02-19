d8663f6df2865b90faf93934248923826057a37d
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2019-02-19 21:09:33 +0100

    StateT based tests

--- src/main/scala/orders/Orders.scala
+++ src/main/scala/orders/Orders.scala
@@ -1,9 +1,21 @@
 package orders
 
+import cats.Monad
+import cats.syntax.all._
 import orders.domain.Order
 
 trait Orders[F[_]] {
   def completeIfPossible(id: Order.Id): F[Option[Order]]
 }
 
-object Orders {}
+object Orders {
+
+  def instance[F[_]: OrderRepository: Monad]: Orders[F] = new Orders[F] {
+    override def completeIfPossible(id: Order.Id): F[Option[Order]] =
+      for {
+        order <- OrderRepository[F].find(id)
+        completeOrder = order.filter(_.status == Order.Status.InProgress).map(_.copy(status = Order.Status.Complete))
+        _ <- completeOrder.map(OrderRepository[F].save).getOrElse(Monad[F].unit)
+      } yield completeOrder.orElse(order)
+  }
+}
--- src/test/scala/orders/OrdersSpec.scala
+++ src/test/scala/orders/OrdersSpec.scala
@@ -1,5 +1,7 @@
 package orders
 
+import cats.Id
+import cats.data.StateT
 import orders.domain.Order
 import orders.domain.Order.Status.Complete
 import orders.domain.Order.Status.InProgress
@@ -9,6 +11,18 @@ import org.scalatest.WordSpec
 
 class OrdersSpec extends WordSpec with Matchers {
 
+  type TestEffect[A] = StateT[Id, Map[Order.Id, Order], A]
+
+  implicit val repository: OrderRepository[TestEffect] = new OrderRepository[TestEffect] {
+    override def find(id: Order.Id): TestEffect[Option[Order]] =
+      StateT.inspect(_.get(id))
+
+    override def save(order: Order): TestEffect[Unit] =
+      StateT.modify(_ + (order.id -> order))
+  }
+
+  val orders = Orders.instance[TestEffect]
+
   val OrderId = Order.Id("123")
   val OrderInInitialStatus = Order(OrderId, Initial)
   val OrderInProgressStatus = Order(OrderId, InProgress)
@@ -16,11 +30,23 @@ class OrdersSpec extends WordSpec with Matchers {
 
   "Order completion" when {
     "order is initial status" should {
-      "not complete the order" in {}
+      "not complete the order" in {
+        val initialState = Map(OrderId -> OrderInInitialStatus)
+
+        val (state, result) = orders.completeIfPossible(OrderId).run(initialState)
+        state shouldEqual initialState
+        result should contain(OrderInInitialStatus)
+      }
     }
 
     "order is in progress" should {
-      "complete order" in {}
+      "complete order" in {
+        val initialState = Map(OrderId -> OrderInProgressStatus)
+
+        val (state, result) = orders.completeIfPossible(OrderId).run(initialState)
+        state shouldEqual Map(OrderId -> OrderInCompleteStatus)
+        result should contain(OrderInCompleteStatus)
+      }
     }
   }
 
