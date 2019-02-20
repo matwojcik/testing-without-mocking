ac3f172ba4b7764bfff8dabf0664cb91cb731a9f
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2019-02-20 20:57:40 +0100

    Orders using PickRepository

--- src/main/scala/orders/Orders.scala
+++ src/main/scala/orders/Orders.scala
@@ -5,6 +5,7 @@ import cats.data.OptionT
 import cats.mtl.FunctorRaise
 import cats.syntax.all._
 import orders.domain.Order
+import orders.domain.Pick
 
 trait Orders[F[_]] {
   def completeOrder(id: Order.Id): F[Order]
@@ -15,17 +16,27 @@ object Orders {
   sealed trait OrderCompletionFailure extends Product with Serializable
   case class NotExistingOrder(id: Order.Id) extends OrderCompletionFailure
   case class OrderNotInProgress(id: Order.Id) extends OrderCompletionFailure
+  case class OrderNotFullyPicked(id: Order.Id) extends OrderCompletionFailure
 
-  def instance[F[_]: OrderRepository: Monad: FunctorRaise[?[_], OrderCompletionFailure]]: Orders[F] = new Orders[F] {
+  def instance[F[_]: OrderRepository: PickRepository: Monad: FunctorRaise[?[_], OrderCompletionFailure]]: Orders[F] = new Orders[F] {
     override def completeOrder(id: Order.Id): F[Order] =
       for {
-        order <- FunctorRaise[F, OrderCompletionFailure].ensure(findOrder(id))(OrderNotInProgress(id))(_.status == Order.Status.InProgress)
+        order <- findOrderForCompletion(id)
+        _     <- verifyPicksAreFinished(id)
         completeOrder = order.copy(status = Order.Status.Complete)
         _ <- OrderRepository[F].save(completeOrder)
       } yield completeOrder
 
+    private def findOrderForCompletion(id: Order.Id) =
+      FunctorRaise[F, OrderCompletionFailure]
+        .ensure(findOrder(id))(OrderNotInProgress(id))(_.status == Order.Status.InProgress)
 
-  private def findOrder(id: Order.Id) =
-    OptionT(OrderRepository[F].find(id)).getOrElseF(FunctorRaise[F, OrderCompletionFailure].raise(NotExistingOrder(id)))
+    private def findOrder(id: Order.Id) =
+      OptionT(OrderRepository[F].find(id)).getOrElseF(FunctorRaise[F, OrderCompletionFailure].raise(NotExistingOrder(id)))
+
+    private def verifyPicksAreFinished(id: Order.Id) =
+      FunctorRaise[F, OrderCompletionFailure]
+        .ensure(PickRepository[F].findPicksOfOrder(id))(OrderNotFullyPicked(id))(_.forall(_.status == Pick.Status.Finished))
+
   }
 }
