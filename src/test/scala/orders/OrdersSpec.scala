0d4791c70f06bfd758a5f7c3464ede44a38fb8cd
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2019-02-20 20:58:01 +0100

    2 fake repositories with TestState

--- src/test/scala/orders/OrdersSpec.scala
+++ src/test/scala/orders/OrdersSpec.scala
@@ -8,24 +8,31 @@ import cats.mtl.MonadState
 import cats.mtl.instances.all._
 import cats.syntax.all._
 import orders.Orders.OrderCompletionFailure
+import orders.Orders.OrderNotFullyPicked
 import orders.Orders.OrderNotInProgress
-import orders.domain.Order
 import orders.domain.Order.Status.Complete
 import orders.domain.Order.Status.InProgress
 import orders.domain.Order.Status.Initial
+import orders.domain.Order
+import orders.domain.Pick
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
 
 class OrdersSpec extends WordSpec with Matchers {
+  case class TestState(orders: Map[Order.Id, Order], picks: Map[Order.Id, List[Pick]])
+  type TestEffect[A] = EitherT[StateT[IO, TestState, ?], OrderCompletionFailure, A]
 
-  type TestEffect[A] = EitherT[StateT[IO, Map[Order.Id, Order], ?], OrderCompletionFailure, A]
-
-  implicit def repository[F[_]: Functor](implicit S: MonadState[F, Map[Order.Id, Order]]): OrderRepository[F] = new OrderRepository[F] {
+  implicit def orderRepository[F[_]: Functor](implicit S: MonadState[F, TestState]): OrderRepository[F] = new OrderRepository[F] {
     override def find(id: Order.Id): F[Option[Order]] =
-      S.get.map(_.get(id))
+      S.get.map(_.orders.get(id))
 
     override def save(order: Order): F[Unit] =
-      S.modify(_ + (order.id -> order))
+      S.modify(s => s.copy(orders = s.orders + (order.id -> order)))
+  }
+
+  implicit def pickRepository[F[_]: Functor](implicit S: MonadState[F, TestState]): PickRepository[F] = new PickRepository[F] {
+    override def findPicksOfOrder(id: Order.Id): F[List[domain.Pick]] =
+      S.get.map(_.picks.getOrElse(id, List.empty))
   }
 
   val orders = Orders.instance[TestEffect]
@@ -35,23 +42,39 @@ class OrdersSpec extends WordSpec with Matchers {
   val OrderInProgressStatus = Order(OrderId, InProgress)
   val OrderInCompleteStatus = Order(OrderId, Complete)
 
+  val PickInProgress = Pick(Pick.Id(1), Pick.Status.InProgress)
+  val PickFinished = Pick(Pick.Id(2), Pick.Status.Finished)
+
   "Order completion" when {
     "order is initial status" should {
       "not complete the order" in {
-        val initialState = Map(OrderId -> OrderInInitialStatus)
+        val initialState = TestState(Map(OrderId -> OrderInInitialStatus), Map.empty)
 
         val (state, result) = orders.completeOrder(OrderId).value.run(initialState).unsafeRunSync()
+
         state shouldEqual initialState
         result should be(Left(OrderNotInProgress(OrderId)))
       }
     }
 
-    "order is in progress" should {
-      "complete order" in {
-        val initialState = Map(OrderId -> OrderInProgressStatus)
+    "order is in progress with in progress picks" should {
+      "not complete order" in {
+        val initialState = TestState(Map(OrderId -> OrderInProgressStatus), Map(OrderId -> List(PickInProgress, PickFinished)))
 
         val (state, result) = orders.completeOrder(OrderId).value.run(initialState).unsafeRunSync()
-        state shouldEqual Map(OrderId -> OrderInCompleteStatus)
+
+        state shouldEqual initialState
+        result should be(Left(OrderNotFullyPicked(OrderId)))
+      }
+    }
+
+    "order is in progress with finished picks" should {
+      "complete order" in {
+        val initialState = TestState(Map(OrderId -> OrderInProgressStatus), Map(OrderId -> List(PickFinished)))
+
+        val (state, result) = orders.completeOrder(OrderId).value.run(initialState).unsafeRunSync()
+
+        state shouldEqual initialState.copy(orders = Map(OrderId -> OrderInCompleteStatus))
         result should be(Right(OrderInCompleteStatus))
       }
     }
